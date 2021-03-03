238ed5d935df0616d3fd130c7b68de0651307a6d
navyadevineni <s538339@nwmissouri.edu>
2021-03-03 15:59:25 -0600

    Added all updated files

---
+++ src/main/java/com/nwmissouri/navyadevineni/App.java
@@ -0,0 +1,14 @@
+
+package com.nwmissouri.navyadevineni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
