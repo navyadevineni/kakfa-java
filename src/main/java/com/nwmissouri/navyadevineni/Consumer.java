3a19670f160a1f66b98ef53cf072edd020acf4f7
navyadevineni <s538339@nwmissouri.edu>
2021-03-03 17:14:57 -0600

    Added 2 new files

---
+++ src/main/java/com/nwmissouri/navyadevineni/Consumer.java
@@ -0,0 +1,32 @@
+package com.nwmissouri.navyadevineni;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        String topicName = "SimpleProducerTopic";
+        String groupName = "SimpleProducerTopicGroup";
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", groupName);
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        consumer.subscribe(Arrays.asList(topicName));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Simple String message= " + String.valueOf(record.value()));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/nwmissouri/navyadevineni/Producer.java
@@ -0,0 +1,33 @@
+package com.nwmissouri.navyadevineni;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class Producer {
+    public static void main(String[] args) throws Exception {
+
+        String topicName = "SimpleProducerTopic";
+        String key = "Key";
+        String value = "Value";
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        for (int i = 0; i <= 100; i++) {
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName, key + i, value + i);
+            // asynchronous send without callback.
+            producer.send(record);
+        }
+
+        producer.close();
+
+        System.out.println("SimpleProducer Completed.");
+    }
+}
